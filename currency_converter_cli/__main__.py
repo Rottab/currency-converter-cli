8cb1528534b0954bb54c67d1f99b90ef5a827afb
Rottab <mr.rottab@gmail.com>
2020-10-03 21:55:37 +0200

    bug fixes

--- currency_converter_cli/__main__.py
+++ currency_converter_cli/__main__.py
@@ -5,18 +5,24 @@ from currency_converter_cli.utils.crud import get_remote_rates_and_store, get_lo
 
 
 def main(argv=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', help='Base currency', type=str, default='EUR')
-    parser.add_argument('-t', help='Currency to convert to', type=str)
-    parser.add_argument('-a', help='Base currency amount', type=int, default=1)
-    parser.add_argument('-d', help='Date of convertion rate in yyyy-mm-dd format',
-                        type=date.fromisoformat, default=None)
+    parser = argparse.ArgumentParser(
+        description='Simple currency converter cli tool based on frankfunrter api')
+    parser.add_argument(
+        '-b', '--base', help='Base currency', metavar='', type=str, default='EUR')
+    parser.add_argument(
+        '-t', '--to', help='Currency to convert to', metavar='', type=str, required=True)
+    parser.add_argument(
+        '-a', '--amount', help='Base currency amount', metavar='', type=int, default=1)
+    parser.add_argument(
+        '-d', '--date', help='Date of convertion rate in yyyy-mm-dd format', metavar='',
+        type=date.fromisoformat, default=None)
+
     args = parser.parse_args(argv)
     return convert(
-        amount=args.a,
-        currency_from=args.f,
-        currency_to=args.t,
-        selected_date=args.d,
+        amount=args.amount,
+        currency_from=args.base.upper(),
+        currency_to=args.to.upper(),
+        selected_date=args.date,
     )
 
 
@@ -25,13 +31,17 @@ def calculate_rate(x, y, amount):
 
 
 def pretty_print(currency_from, currency_to, amount, calculated_amount, selected_date):
+    if selected_date >= date.today():
+        selected_date = 'latest'
     print(f'{amount} {currency_from} is equal to {calculated_amount} {currency_to} based on {selected_date} rates.')
 
 
 def convert(currency_to, currency_from=None, amount=1, selected_date=None):
     # Validate parameters
-
-    # Check if exits in database
+    if selected_date:
+        if selected_date > date.today() or selected_date < date(year=1999, month=1, day=4):
+            raise Exception('Invalid date')
+    # Check if entry exits in database
     rates = get_local_rates(currency_from, currency_to, selected_date)
     # If not. Then request from the api and save it locally
     if not rates:
@@ -49,7 +59,7 @@ def convert(currency_to, currency_from=None, amount=1, selected_date=None):
         currency_to=currency_to,
         amount=amount,
         calculated_amount=calculated_amount,
-        selected_date=selected_date or 'latest'
+        selected_date=selected_date or date.today()
     )
 
 
--- currency_converter_cli/utils/crud.py
+++ currency_converter_cli/utils/crud.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import date
+from datetime import date, timedelta
 from pony.orm import db_session, select, desc
 from currency_converter_cli.utils.db import Dates, Rates
 
@@ -17,7 +17,7 @@ def get_local_rates(currency_from, currency_to, selected_date):
     if not date_result:
         return False
     # Return nothing if the top most entry is outdated
-    if date_result['date'] < date.today():
+    if date_result['date'] < (date.today() - timedelta(days=1)):
         return False
     # Get 'from' rate based on the latest date
     rate_from = None
@@ -29,11 +29,14 @@ def get_local_rates(currency_from, currency_to, selected_date):
             rate_from = rate_result.to_dict()['rate']
     # Get 'to' rate based on latest date
     rate_to = None
-    rate_result = Rates.get(code=currency_to, date=date_result['id'])
-    if rate_result:
-        rate_to = rate_result.to_dict()['rate']
+    if currency_to in [None, 'EUR']:
+        rate_to = 1
     else:
-        raise Exception('CODE IS INCORRECT YOU PIECE OF SHIT')
+        rate_result = Rates.get(code=currency_to, date=date_result['id'])
+        if rate_result:
+            rate_to = rate_result.to_dict()['rate']
+        else:
+            raise Exception('CODE IS INCORRECT YOU PIECE OF SHIT')
 
     return {
         'rate_from': rate_from,
@@ -44,7 +47,7 @@ def get_local_rates(currency_from, currency_to, selected_date):
 def _rates_from_json(currency_from, currency_to, json):
     return {
         'rate_from': json['rates'][currency_from] if currency_from not in [None, 'EUR'] else 1,
-        'rate_to': json['rates'][currency_to],
+        'rate_to': json['rates'][currency_to] if currency_to not in [None, 'EUR'] else 1,
     }
 
 
@@ -55,7 +58,6 @@ def get_remote_rates_and_store(currency_from, currency_to, selected_date=None):
             f'https://api.frankfurter.app/{selected_date}').json()
         query_result = Dates.get(date=selected_date)
         if query_result:
-            print('DATE IS ALREADY IN THE DATABASE YOU GOOL')
             return _rates_from_json(currency_from, currency_to, json)
 
         date_obj = Dates(base=json['base'], date=json['date'])
